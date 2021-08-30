95a901f4538e0251c558fd80a2ae2a01d9a1f6c0
yhj <tntj123@naver.com>
2021-08-30 20:26:55 +0900

    메인 페이지 생성 완료

---
+++ src/mainPage/MainClass.java
@@ -0,0 +1,30 @@
+package mainPage;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class MainClass {
+	public static void main(String[] args) {
+		HashMap<String, STU> hm = new HashMap<String, STU>();
+		Scanner scan = new Scanner(System.in);
+		String name, stNum;
+		int num, PhoneNo;
+		System.out.println("--학생 관리 프로그램 시작--");
+		while (true) {
+			System.out.println("1.생성 2.전체보기 3.하나만 보기 4.수정 5.삭제");
+			num = scan.nextInt();
+			switch (num) {
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			}
+		}
+	}
+}
---
+++ src/mainPage/STU.java
@@ -0,0 +1,31 @@
+package mainPage;
+
+public class STU {
+	private String name;
+	private String stuNo;
+	private int PhoneNo;
+
+	public int getPhoneNo() {
+		return PhoneNo;
+	}
+
+	public void setPhoneNo(int phoneNo) {
+		PhoneNo = phoneNo;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getStuNo() {
+		return stuNo;
+	}
+
+	public void setStuNo(String stuNo) {
+		this.stuNo = stuNo;
+	}
+}
